3ec58f3d10b8ec8654f4cc1714f903f4dbb0d72f
dhwlxn <dhwlxn@naver.com>
2023-03-31 16:09:57 +0900

    createW error

--- 1410002/user.c
+++ 1410002/user.c
@@ -1,30 +1,8 @@
-#include <stdio.h>
 #include "user.h"
-#include "screen.h"
-
-#include <stdlib.h>
-#include <string.h>
-#include <conio.h>		// 방향키 입력값위한 _kbhit() 의 헤더파일 선언
-//void login() {
-//	MainScreen();
-//}
 
 #define LEFT 75			// 방향키별 아스키코드값
 #define RIGHT 77
 
-void Login();
-void SignUp();
-void SignIn();
-
-
-int main()
-{
-	// test용
-	printf("왼쪽, 오른쪽 방향키를 눌러주세요. \n");
-	Login();
-
-	return 0;
-}
 
 void Login() 
 {
