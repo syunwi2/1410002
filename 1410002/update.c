4bdf9c971217299d68e48f0d376d724ad6ccee54
koyohan <hphk.rdg9074@gmail.com>
2023-04-02 09:29:50 +0900

    update_2

--- 1410002/update.c
+++ 1410002/update.c
@@ -7,12 +7,15 @@ void UpdateEvent(EVENT* root, EVENT* node)
 	printf("어떤 일정을 수정하시겠습니까? \n");
 	//일정 전체 출력
 	PersonalReadEvent(root);
-	
+
 	//수정 노드 선택
 
+
+
 	//개인파일에서 수정
-	
-
-	//팀파일에서 수정
-
+//	CreateNewEvent(root, id, start, end, title, tag, isPublic, imPortanceLevel);
+//	DeleteEvent(root, node);
+//	//팀파일에서 수정
+//	CreateNewEvent(root, id, start, end, title, tag, isPublic, imPortanceLevel);
+//	DeleteEvent(root, node);
 }
