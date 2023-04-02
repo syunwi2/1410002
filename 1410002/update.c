9cd8dd9970fdb2164c568ad713017e26d5c6b1ec
unknown <rdg9074@gmail.com>
2023-04-03 08:30:50 +0900

    read_4

--- 1410002/update.c
+++ 1410002/update.c
@@ -1,14 +1,25 @@
 #include "update.h"
 
-void UpdateEvent(EVENT* root, EVENT* node)
+void UpdateEvent(EVENT** root, EVENT* node)
 {
-	EVENT* ptr;
+	int chk;
+	EVENT* ptr = node;
 	printf("일정을 수정 할 수 있습니다. \n");
 	printf("어떤 일정을 수정하시겠습니까? \n");
 	//일정 전체 출력
-	PersonalReadEvent(root);
+	do
+	{
+		printf("일정 다시 확인하기 y or n");
+		scanf_s("%d", &chk);
+	} while (chk != 1 && chk != 2);
+	if (chk == 1)
+	{
+		PersonalReadEvent(root);
+	}
 	
 	//수정 노드 선택
+
+
 
 	//개인파일에서 수정
 	
