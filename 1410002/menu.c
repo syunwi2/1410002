cec60937890f100aef6399b6b5312e7ed099c590
오지원 <dhwlxn@naver.com>
2023-04-04 00:22:07 +0900

    출력화면 수정
    main
    menu
    screen
    signin
    signup

--- 1410002/menu.c
+++ 1410002/menu.c
@@ -13,16 +13,28 @@ void Menu(EVENT** personalRoot, EVENT** teamRoot, PERSON* user_ptr)
 	gotoxy(5, 18);
 	do
 	{
-	printf("\n            MENU  \n\n");
-	printf("         1. 일정 생성  \n");
-	printf("         2. 개인 일정 조회  \n");
-	printf("         3. 팀 일정 조회  \n");
-	printf("         4. 팀 일정 수정  \n");
-	printf("         5. 팀 일정 삭제  \n");
-	printf("         6. 종료      \n");
+		textcolor(14);
+		gotoxy(27, 13);
+		printf("==================================");
+		gotoxy(27, 14);
+		printf("               MENU         ");
+		gotoxy(27, 15);
+		printf("==================================");
 
-	printf("\n\n  ▶ 원하는 작업번호를 입력해주세요. : ");
-	scanf("%d%*c", &ans);
+		textcolor(15);
+		gotoxy(27, 17);
+		printf("         1. 일정 생성  ");
+		gotoxy(27, 18);
+		printf("         2. 개인 일정 조회  ");
+		gotoxy(27, 19);
+		printf("         3. 팀 일정 조회  ");
+		gotoxy(27, 20);
+		printf("         4. 종료      ");
+
+		gotoxy(26, 23);
+		textcolor(14);
+		printf("▶ 원하는 작업번호를 입력해주세요. : ");
+		scanf("%d%*c", &ans);
 
 
 		switch (ans)
