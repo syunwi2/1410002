62fa48c36fd83751b99eda509d5c61069fdfc091
Dongmyeongleee <aklsjd7380@gmail.com>
2023-04-03 09:32:33 +0900

    불필요한 파일 제거

--- pjt/fileio.h
+++
@@ -1,9 +0,0 @@
-#include "schedule.h"
-#include "user.h"
-#include <string.h>
-#include <stdlib.h>
-
-void PublicFileSave(void);
-struct EVENT* PublicFileLoad(void);
-void PrivateFileSave(PERSON* User);
-EVENT* PrivateFileLoad(PERSON* User);
