bcbe13beb8c1c98efa46ab0f3be5748ac0a3383d
dhwlxn <dhwlxn@naver.com>
2023-04-04 12:07:09 +0900

    read 수정

--- 1410002/read.c
+++ 1410002/read.c
@@ -83,7 +83,7 @@ void PersonalReadEvent(EVENT** root)
     
     if ((*root) == NULL)
     {
-        printf("\n                         일정이 존재하지 않습니다.");
+        printf("\n                             일정이 존재하지 않습니다.");
         return;
     }
 
@@ -98,33 +98,30 @@ void PersonalReadEvent(EVENT** root)
         printf("==================================");
 
         textcolor(15);
+        gotoxy(27, 17);
+        printf("%s님의 개인 일정을 출력하세요.", (*root)->ownerID);
+        gotoxy(27, 18);
+        printf("                                            ");
+        gotoxy(27, 19);
+        printf("       1.  전체 일정 출력      ");
+        gotoxy(27, 20);
+        printf("       2.  개인 일정 출력       ");
+        gotoxy(27, 21);
+        printf("       3.  회사 일정 출력       ");
+        gotoxy(27, 22); 
+        printf("       4.  기타 일정 출력      ");
+        gotoxy(0, 23);
+        printf("                                                                                                         ");
+        gotoxy(27, 23);
+        printf("       5.  조회 종료                         \n");
+        printf("                                                                           ");
+
+        gotoxy(27, 25);
+        textcolor(14);
+        printf("▶ 원하는 작업번호를 입력해주세요. : ");
         do 
         {
-
-            gotoxy(27, 17);
-            printf("%s님의 개인 일정을 출력하세요.", (*root)->ownerID);
-            gotoxy(27, 18);
-            printf("                                            ");
-            gotoxy(27, 19);
-            printf("       1.  전체 일정 출력      ");
-            gotoxy(27, 20);
-            printf("       2.  개인 일정 출력       ");
-            gotoxy(27, 21);
-            printf("       3.  회사 일정 출력       ");
-            gotoxy(27, 22); 
-            printf("       4.  기타 일정 출력      ");
-            gotoxy(0, 23);
-            printf("                                                                                                         ");
-            gotoxy(27, 23);
-            printf("       5.  조회 종료                         \n");
-            printf("                                                                           ");
-
-            gotoxy(27, 25);
-            textcolor(14);
-            printf("▶ 원하는 작업번호를 입력해주세요. : ");
             printf("                    \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
-
-
             scanf_s("%d%*c", &choice);
         } while (choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5);
 
@@ -213,6 +210,7 @@ void PersonalReadEvent(EVENT** root)
             int findnum;
             char findid[7] = { 0 };
             printf("\n\n                             ▶ 삭제할 노드 번호 선택 : ");
+            printf("                    \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
             scanf_s("%d%*c", &findnum);
             sprintf(findid, "%.6d", findnum);
 
